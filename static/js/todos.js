179a6b485857a6974ec1579d92ae1151fefd0091
fabricev <fabrice.vergnenegre@sokube.ch>
2020-06-05 08:28:40 -0700

    FIX: API calls relative paths

--- static/js/todos.js
+++ static/js/todos.js
@@ -1,6 +1,6 @@
 
 const createNewTask = async () => {
-    const response = await fetch('/api/v1/tasks', {
+    const response = await fetch('./api/v1/tasks', {
         method: 'POST',
         body: JSON.stringify( { description: document.getElementById("task_description").value }),
         headers: { 'Content-Type': 'application/json' }
@@ -10,7 +10,7 @@ const createNewTask = async () => {
 }
 
 const completeTask = async (index) => {
-    const response = await fetch('/api/v1/tasks/'+index+"/toggle", {
+    const response = await fetch('./api/v1/tasks/'+index+"/toggle", {
         method: 'POST'
     });
     const myJson = await response.json(); //extract JSON from the http response
@@ -18,7 +18,7 @@ const completeTask = async (index) => {
 }
 
 const deleteTask = async (index) => {
-    const response = await fetch('/api/v1/tasks/'+index, {
+    const response = await fetch('./api/v1/tasks/'+index, {
         method: 'DELETE',
         body: JSON.stringify( { id: index }),
         headers: {'Content-Type': 'application/json'}
